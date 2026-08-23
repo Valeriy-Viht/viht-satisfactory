5429afdc59c6ec9e31d24075ae3ad27b576c7631
Valeriy Viht <ffodevilsusij@gmail.com>
2026-08-23 07:22:32 +0300

    refactor: add return type to the IMiddleware interface

--- backend/src/Modules/Http/Contracts/IMiddleware.php
+++ backend/src/Modules/Http/Contracts/IMiddleware.php
@@ -5,5 +5,5 @@ namespace App\Modules\Http\Contracts;
 use Workerman\Protocols\Http\Request;
 
 interface IMiddleware {
-    public function process(Request $request);
+    public function process(Request $request): Request;
 }
