d5a3eb94d686aae841e8857b5c85909e5a844737
Valeriy Viht <ffodevilsusij@gmail.com>
2026-08-23 07:59:06 +0300

    refactor: remove the DependencyResolver implementation overhead and move resolve mrthod to the Container class

--- backend/src/Modules/DI/Container.php
+++ backend/src/Modules/DI/Container.php
@@ -6,6 +6,7 @@ use App\Utils\TypeAssert;
 use Closure;
 use InvalidArgumentException;
 use LogicException;
+use ReflectionClass;
 
 class Container {
 
@@ -16,7 +17,7 @@ class Container {
     public function registrate(string $class, ?Closure $factory = null) {
         TypeAssert::exists($class);
 
-        $factory = is_callable($factory) ? $factory : fn ($sc) => DependencyResolver::resolve($sc, $class);
+        $factory = is_callable($factory) ? $factory : fn ($sc) => $this->resolve($sc, $class);
 
         if (!is_callable($factory)) {
             throw new InvalidArgumentException("Provided closure isn't callable");
@@ -66,5 +67,25 @@ class Container {
     public function has(string $class): bool {
         TypeAssert::exists($class);
         return isset($registrations[$class]);
+    }
+
+
+    private function resolve(Container $c, string $baseClass) {
+
+        $instances = [];
+        $refClass = new ReflectionClass($baseClass);
+        $constructor = $refClass->getConstructor();
+        
+        if (isset($constructor)) {
+            $deps = $constructor->getParameters();
+            foreach ($deps as $depClass) {
+                $depClass = $depClass->getType()->getName();
+                TypeAssert::exists($depClass);
+                $instances[] = $c->get($depClass);
+            }
+        }
+        
+
+        return new $baseClass(...$instances);
     }
 }
--- backend/src/Modules/DI/DependencyResolver.php
+++ backend/src/Modules/DI/DependencyResolver.php
@@ -7,22 +7,5 @@ use ReflectionClass;
 
 class DependencyResolver {
     
-    public static function resolve(Container $c, string $baseClass) {
-
-        $instances = [];
-        $refClass = new ReflectionClass($baseClass);
-        $constructor = $refClass->getConstructor();
-        
-        if (isset($constructor)) {
-            $deps = $constructor->getParameters();
-            foreach ($deps as $depClass) {
-                $depClass = $depClass->getType()->getName();
-                TypeAssert::exists($depClass);
-                $instances[] = $c->get($depClass);
-            }
-        }
-        
-
-        return new $baseClass(...$instances);
-    }
+    
 }
