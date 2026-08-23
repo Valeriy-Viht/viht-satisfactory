540357961ba59a6af1b67f53b152cd749b5efe31
Valeriy Viht <ffodevilsusij@gmail.com>
2026-08-23 06:56:31 +0300

    fix(router): fix creating middleware for route resolve (use singleton for non-state middleware type)

--- backend/src/Modules/Http/Router/Router.php
+++ backend/src/Modules/Http/Router/Router.php
@@ -39,6 +39,7 @@ class Router {
 
         if (count($middlewares) > 0) {
             foreach ($middlewares as $middleware) {
+                $middleware = $this->container->get($middleware);
                 $request = $middleware->process($request);
             }
         }
